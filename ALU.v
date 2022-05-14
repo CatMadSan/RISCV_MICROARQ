e4751fa5dc0ce6f6d28e31c04ac63773a9de2c04
CatMadSan <99616267+CatMadSan@users.noreply.github.com>
2022-05-14 01:29:06 -0600

    Update ALU.v
    Editado para que coincida con la version final de la microarquitectura.

--- ALU.v
+++ ALU.v
@@ -1,28 +1,6 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/11/2022 06:34:04 PM
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALU
     #(parameter N = 3)(
-    input [3:0] OP, //Entra ALUCntrl, que para fines de este proyecto, debe representar los 5 (=4'b101 > 3'b11) estados diferentes
+    input [2:0] OP, //Entra ALUCntrl, que para fines de este proyecto, debe representar los 5 (=4'b101 > 3'b11) estados diferentes
     input [N:0] Operand1,//Entra ALUSrc1
     input [N:0] Operand2,//ALUSrc2
     output [N:0] Out//Sale ALUResult
@@ -36,7 +14,6 @@ module ALU
             1: resultado <= Operand1 >> Operand2;//Caso 001: bitwise shift right
             2: resultado <= Operand1 | Operand2;//Caso 010: bitwise or
             3: resultado <= Operand1 & Operand2;//Caso 011: bitwise and
-            4: resultado <= Operand1 << Operand2; //Caso 100: bitwise shift left, el menos ussado (usado SOLO con LUI)
         endcase
     end
     
