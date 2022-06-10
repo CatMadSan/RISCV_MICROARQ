b46ee308a1710f4f0640ffef80283b5809f71d80
CatMadSan <99616267+CatMadSan@users.noreply.github.com>
2022-06-10 12:48:38 -0600

    Update MEMORY_TB.v

--- SourceRISCV/MEMORY_TB.v
+++ SourceRISCV/MEMORY_TB.v
@@ -1,26 +1,48 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/10/2022 12:39:57 PM
-// Design Name: 
-// Module Name: MEMORY_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module MEMORY_TB();
+//Inputs 
+   reg clock;
+   reg Reset;
+   reg [31:0] Dir;
+   reg [31:0] WriteData;
+   reg MemWrite;
 
-module MEMORY_TB(
+integer i;
 
+//Outputs 
+reg [31:0] ReadData;
+
+DataM UUT(
+    .clock(clock),
+    .Reset(Reset),
+    .Dir(Dir),
+    .WriteData(WriteData),
+    .MemWrite(MemWrite),
+    .ReadData(ReadData)
     );
+
+initial begin
+ forever #20 clock <= ~clock;
+ end
+ always@(posedge clock) begin
+  for (i=0; i<10; i=i+1)begin
+		Reset<=$random;
+		Dir<=$random;
+		WriteData<=$random;
+		MemWrite<=$random;
+		#20;
+		end
+	  $dumpfile("testmemoria.txt");
+	  $dumpvars(1,Dir,ReadData); 
+	  #200;
+        $dumpoff;
+        #200;
+        $dumpon;
+        #20;
+        $dumpall;
+        #10;
+        $dumpflush;
+
+end
 endmodule
